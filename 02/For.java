ae42a7e6573a05aa54436fbdccf83f678107c7e7
masagao <masanao@gaogao.asia>
2020-09-27 13:21:41 +0900

    Commit prePractice

---
+++ 02/For.java
@@ -0,0 +1,9 @@
+class For {
+  public static void main(String[] args) {
+    int i = 100;
+    if(i < 100)
+      System.out.println("i は100未満です");
+    else
+      System.out.println("i は100以上です");
+  }
+}
---
+++ 02/Operator.java
@@ -0,0 +1,5 @@
+class Operator {
+  public static void main(String[] args) {
+    System.out.println("金額は¥1,000 です");
+  }
+}
