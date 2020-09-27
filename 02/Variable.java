f9e176169a90934f543c1ff09e59d7b747cdddeb
masagao <masanao@gaogao.asia>
2020-09-27 13:57:26 +0900

    Commit Practice

---
+++ 02/Variable.java
@@ -0,0 +1,11 @@
+class Variable {
+  public static void main(String[] args) {
+    int a = 20;
+    double b = 0.5;
+    double c = a * b;
+    int d = (int)(a * b);
+    System.out.println(c);
+    System.out.println((int)b);
+    System.out.println(d);
+  }
+}
