66ba19a9182a548bcaf1127ed803e62d6fa13c70
masagao <masanao@gaogao.asia>
2020-09-27 13:44:04 +0900

    Commit Practice

--- 02/Operator.java
+++ 02/Operator.java
@@ -1,5 +1,13 @@
 class Operator {
   public static void main(String[] args) {
-    System.out.println("金額は¥1,000 です");
+    short count = 0;
+    int average =  (int)-7.54;
+    String name = "teikyo";
+    String str = ":";
+    char[] splitter = str.toCharArray();
+    System.out.println(count);
+    System.out.println(average);
+    System.out.println(name);
+    System.out.println(splitter);
   }
 }
