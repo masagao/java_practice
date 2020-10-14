f9e5c32a47c764e057910f1e767a5cf66722bd26
masagao <masanao@gaogao.asia>
2020-10-15 02:08:42 +0900

    Fix interface

--- 10/Kinko.java
+++ 10/Kinko.java
@@ -1,23 +1,30 @@
-public class Kinko implements Lockable {
-  boolean isLock = false;
-  int deposit = 0;
+public class Kinko implements Lockable{
+    boolean isLock = false;
+    int deposit = 0;
 
-  public Kinko(int d) {
-    deposit = d;
-  }
+    public Kinko(int d){
+        deposit = d;
+    }
 
-  public boolean lock() {
-    isLock = true;
-    return isLock;
-  }
+    public boolean lock(){
+        isLock = true;
+        return isLock;
+    }
 
-  public boolean unlock() {
-    isLock = false;
-    return isLock;
-  }
+    public boolean unlock(){
+        isLock = true;
+        return isLock;
+    }
 
-  void printDeposit() {
-    if(isLock == true) System.out.println("ロックされています");
-    else System.out.println("預金額は" + deposit + "です");
-  }
+    public boolean unlock(int t){
+        if(deposit >= t){
+            isLock = true;
+        }
+        return isLock;
+    }
+
+    void printDeposit (){
+        if(isLock == true)System.out.println("ロックされています");
+        else System.out.println("預金額は"+deposit+"です");
+    }
 }
--- 10/Lockable.java
+++ 10/Lockable.java
@@ -1,4 +1,4 @@
 public interface Lockable {
-  boolean lock();
-  boolean unlock();
+    boolean lock();
+    boolean unlock();
 }
