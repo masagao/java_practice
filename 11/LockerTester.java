d6f08c152e0bd03ebf8d3ff77b4eecc822d784d8
masagao <masanao@gaogao.asia>
2020-10-20 22:43:56 +0900

    Add implements

--- 11/LockerTester.java
+++ 11/LockerTester.java
@@ -3,18 +3,13 @@ class LockerTester {
     Kinko k = new Kinko(1000);
     LockableJewelBox j = new LockableJewelBox(false);
 
+    k.lock(new KeyWord1());
+    j.lock(new KeyWord2());
+
+    k.unlock(new KeyWord1());
+    j.unlock(new KeyWord2());
+
     PrintState[] state = {k, j};
-    for(PrintState s: state) {
-      s.print();
-    }
-
-    // Lockable[] state = {k, j};
-    // for(Lockable s: state) {
-    //   s.lock();
-    // }
-
-    // for(PrintState s: state) {
-    //   s.print();
-    // }
+    for(PrintState s: state) s.print();
   }
 }
--- 11/Test.java
+++
@@ -1,7 +0,0 @@
-import tools.*;
-
-class Test implements MyKey {
-  public String print() {
-    return "1";
-  };
-}
